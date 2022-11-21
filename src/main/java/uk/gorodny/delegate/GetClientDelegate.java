0d0440c104783d9931fde2dbfb6a9a9b736ccbfa
gorodnyuk <sergeygorodnyuk@yandex.ru>
2022-11-21 09:46:55 +0300

    Добавил обработчик для http-connector

--- src/main/java/uk/gorodny/delegate/GetClientDelegate.java
+++ src/main/java/uk/gorodny/delegate/GetClientDelegate.java
@@ -2,19 +2,44 @@ package uk.gorodny.delegate;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.connect.Connectors;
+import org.camunda.connect.httpclient.HttpConnector;
+import org.camunda.connect.httpclient.HttpRequest;
+import org.camunda.connect.httpclient.HttpResponse;
 import org.springframework.stereotype.Component;
+import uk.gorodny.domain.Client;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class GetClientDelegate implements JavaDelegate {
 
-    private final String uri;
+    private final String url;
 
-    public GetClientDelegate(String uri) {
-        this.uri = uri;
+    public GetClientDelegate(String url) {
+        this.url = url;
     }
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+        Client client = null;
 
+        HttpConnector httpConnector = Connectors.getConnector(HttpConnector.ID);
+        HttpRequest request = httpConnector.createRequest()
+                .url(url)
+                .get();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-type", "application/json");
+
+        request.setRequestParameter("headers", headers);
+
+        HttpResponse response = request.execute();
+        if (response.getStatusCode() == 200 || !response.getResponse().isEmpty()) {
+            // Нужен маппинг на клиента
+        }
+        response.close();
+
+        delegateExecution.setVariable("client", client);
     }
 }
